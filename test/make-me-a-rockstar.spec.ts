0715e9b77f95a453986b5f53bed179a0366dcf5d
Aung Myo Kyaw <git@aungmyokyaw.com>
2019-05-06 22:26:03 +0630

    fix: test

--- test/make-me-a-rockstar.spec.ts
+++ test/make-me-a-rockstar.spec.ts
@@ -4,16 +4,16 @@ import * as fs from 'fs';
 
 import 'mocha';
 
-const cliPath = `${process.env.PWD}/src/cli.ts`;
+const cliPath = `${process.env.PWD}/bin/src/cli.js`;
 
 describe('MAKE-ME-A-ROCKSTAR', () => {
   it('SHOULD RETURN HELP', async () => {
-    const { stdout } = await execa.shell(`ts-node ${cliPath} --help`);
+    const { stdout } = await execa.shell(`node ${cliPath} --help`);
     assert.equal(stdout.includes('Examples'), true);
   });
   it('SHOULD MAKE YOU ROCKSTAR', async () => {
     const { stdout } = await execa.shell(
-      `ts-node ${cliPath} --lang='javascript' --day='1' --month='1' year='1'`
+      `node ${cliPath} --lang='javascript' --day='1' --month='1' year='1'`
     );
     assert.equal(fs.existsSync('make-me-a-rockstar'), true);
   });
